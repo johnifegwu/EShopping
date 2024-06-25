0921ff2820d2754c572a2160b7eb90a3ce8f9540
John Ifegwu <57634092+johnifegwu@users.noreply.github.com>
2024-06-25 16:17:09 +0100

    Creating Product Query endpoints

---
+++ Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,66 @@
+﻿using Catalog.Application.Queries.Products;
+using Catalog.Application.Responses;
+using MediatR;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Net;
+
+namespace Catalog.API.Controllers
+{
+    /// <summary>
+    /// Product Catalog Controller.
+    /// </summary>
+    public class CatalogController : BaseController
+    {
+        private readonly IMediator _mediator;
+
+        public CatalogController(IMediator mediator)
+        {
+            this._mediator = mediator;
+        }
+
+        /// <summary>
+        /// Gets all the Products from the system.
+        /// </summary>
+        /// <param name="pageIndex">Page index (default is 1).</param>
+        /// <param name="pageSize">Page size (default is 15).</param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("[action]/", Name = "GetAllProducts")]
+        [ProducesResponseType(typeof(List<ProductResponse>), (int)HttpStatusCode.OK)]
+        [SwaggerOperation(Tags = new[] { "eShopping Catalog | Products Query" })]
+        public async Task<ActionResult> GetAllProducts(
+            [FromQuery]int pageIndex = 1, 
+            [FromQuery]int pageSize = 15)
+        {
+            var result = await _mediator.Send(new GetAllProductsQuery
+            {
+                PageIndex = pageIndex,
+                PageSize = pageSize
+            });
+            return Ok(result);
+        }
+
+
+        /// <summary>
+        /// Gets Product from the system by Id.
+        /// </summary>
+        /// <param name="Id">Id of the product to return.</param>
+        /// <returns>ProductResponse</returns>
+        [HttpGet]
+        [Route("[action]/{Id}", Name = "GetProductById")]
+        [ProducesResponseType(typeof(ProductResponse), (int)HttpStatusCode.OK)]
+        [SwaggerOperation(Tags = new[] {"eShopping Catalog | Products Query"})]
+        public async Task<ActionResult> GetProductById(string Id)
+        {
+            var result = await _mediator.Send(new GetProductByIdQuery 
+            { 
+                Id = Id 
+            });
+
+            return Ok(result);
+        }
+
+    }
+}
--- Catalog.API/Program.cs
+++ Catalog.API/Program.cs
@@ -1,15 +1,51 @@
+using Asp.Versioning;
+using Catalog.Application.Commands.Brands;
+using Catalog.Core.Entities;
 using Catalog.Infrastructure.Extensions;
 using Catalog.Infrastructure.Seeders;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+//=====================================================================================================
 builder.Services.AddControllers();
+builder.Services.AddApiVersioning(o =>
+{
+    o.ReportApiVersions = true;
+    o.AssumeDefaultVersionWhenUnspecified = true;
+    o.DefaultApiVersion = new ApiVersion(1, 0);
+});
+
+var envVariable = builder.Configuration["ConnectionStrings:EnvironmentVariable"];
+var conString = Environment.GetEnvironmentVariable(envVariable);
+builder.Services.AddHealthChecks().AddMongoDb(conString, "Catalog MongoDb Helth Check", HealthStatus.Degraded);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(x =>
+{
+    x.EnableAnnotations();
+    x.SwaggerDoc("v1", new OpenApiInfo() { Title = "eShopping Catalog ® API", Version = "v1" });
+});
+//=====================================================================================================
+
+// Add depencies
+//=====================================================================================================
+var mediatRAssemblies = new[]
+{
+  Assembly.GetAssembly(typeof(ProductType)), // Core
+  Assembly.GetAssembly(typeof(CreateBrandCommand)) // Application
+};
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(mediatRAssemblies!));
 builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddAutoMapper(typeof(Program));
+//=====================================================================================================
 
 var app = builder.Build();
 var scope = app.Services.CreateScope();
@@ -20,13 +56,23 @@ await seed.Seed();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(x =>
+    {
+        x.RoutePrefix = "swagger";
+        x.SwaggerEndpoint("/swagger/v1/swagger.json", "eShopping Catalog ® API");
+    });
     app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
 
 app.UseRouting();
+
+app.MapHealthChecks("/health", new HealthCheckOptions
+{
+    Predicate = _ => true,
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
 
 app.UseStaticFiles();
 
