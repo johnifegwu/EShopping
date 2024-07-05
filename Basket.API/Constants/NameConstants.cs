1e07811e3cf1aaa2862fd2fbeae904e733860bba
John Ifegwu <57634092+johnifegwu@users.noreply.github.com>
2024-07-05 08:15:19 +0100

    Creating Basket Microservice

---
+++ Basket.API/Constants/NameConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace Basket.API.Constants
+{
+    public class NameConstants
+    {
+        public const string ProductQuerySwaggerName = "eShopping Basket | Product Query";
+
+        public const string ProductCommandSwaggerName = "eShopping Basket | Product Command";
+    }
+}
---
+++ Basket.API/Controllers/BaseController.cs
@@ -0,0 +1,12 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.API.Controllers
+{
+    [ApiVersion("1")]
+    [Route("api/v{version.apiversion}/[controller]")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+    }
+}
