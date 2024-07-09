ab01971241bd20c42dbe6c0910ac028d92d35f45
John Ifegwu <57634092+johnifegwu@users.noreply.github.com>
2024-07-09 16:53:08 +0100

    Creating Discount Microservice using CLEAN architecture

---
+++ Discount.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿
+using Data.Repositories;
+using Discount.Infrastructure.Persistence;
+using Discount.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Discount.Infrastructure.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            var conString = configuration["ConnectionStrings:DiscountDbConnection"];
+            services.AddDbContext<DiscountDbContext>(options => options.UseNpgsql(conString), ServiceLifetime.Transient);
+            services.AddTransient<IUnitOfWorkCore, UnitOfWorkDiscount>();
+        }
+    }
+}
---
+++ Discount.Infrastructure/Persistence/DiscountDbContext.cs
@@ -0,0 +1,22 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Infrastructure.Persistence
+{
+    internal class DiscountDbContext : DbContext
+    {
+
+        public DiscountDbContext(DbContextOptions<DiscountDbContext> options): base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //ToDo: Apply table configurations here
+
+        }
+    }
+}
