5cb1a044463fa9a7193443ac59716808ec192f21
John Ifegwu <57634092+johnifegwu@users.noreply.github.com>
2024-07-26 23:33:42 +0100

    Gateway API fixed

--- Gateway/Gateway.API/Program.cs
+++ Gateway/Gateway.API/Program.cs
@@ -1,18 +1,38 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.RateLimiting;
+using Microsoft.IdentityModel.Tokens;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer();
+// Configure JWT Authentication
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
 
 builder.Services.AddAuthorization(options =>
 {
-    options.AddPolicy("authenticated", policy =>
-        policy.RequireAuthenticatedUser());
+    options.AddPolicy("AdminOrCustomer", policy => policy.RequireRole("Admin", "Customer"));
 });
 
 builder.Services.AddRateLimiter(rateLimiterOptions =>
@@ -26,7 +46,15 @@ builder.Services.AddRateLimiter(rateLimiterOptions =>
 
 builder.Services
     .AddReverseProxy()
+    .ConfigureHttpClient((context, options) =>
+    {
+        options.SslOptions = new SslClientAuthenticationOptions
+        {
+            RemoteCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true
+        };
+    })
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
 
 var app = builder.Build();
 
