47c2684c8ca78e80946975037390bafcf2ea1c55
John Ifegwu <57634092+johnifegwu@users.noreply.github.com>
2024-08-16 20:14:56 +0100

    Made Jay.Cahce and Jay.EFCore independent of local libraries

---
+++ Libraries/EFCore.UnitOfWork/Interfaces/IJayIDbContext .cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace EFCore.UnitOfWorkCore.Interfaces
+{
+    public interface IJayDbContext : IDisposable
+    {
+        DbSet<TEntity> Set<TEntity>() where TEntity : class;
+        DatabaseFacade Database { get; }
+        int SaveChanges();
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ Libraries/EFCore.UnitOfWork/Interfaces/IUnitOfWorkCore.cs
@@ -0,0 +1,12 @@
+﻿
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace Data.Repositories
+{
+    public interface IUnitOfWorkCore : IDisposable
+    {
+        IRepository<T> Repository<T>() where T : class;
+        DatabaseFacade GetDatabase();
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
